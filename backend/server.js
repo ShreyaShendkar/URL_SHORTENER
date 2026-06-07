8d79ce48131c06b5bf9e77f7e96a0b88eb9277a8
ShreyaShendkar <shreyashendkar8@gmail.com>
2026-06-07 11:05:56 +0530

    feat: implement URL shortening and retrieval functionality

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,31 @@
-import express from 'express'
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import urlRoutes from './routes/urlRoutes.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET","POST"]
+ })
+);
+app.use(express.json());
+app.use("/",urlRoutes);
+
+
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log("Connected to MongoDB");
+  app.listen(process.env.PORT, () => {
+  console.log(`Server is running successfully on port ${process.env.PORT}`)
+});
+})
+.catch((err) => {
+  console.log("Error connecting to MongoDB: ",err);
+});
